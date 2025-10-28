5748bf7fd641548f698b801d632b4f2cc422d418
nahaz <shaiknahaz@gmail.com>
2025-10-28 16:35:55 +0530

    firstcommit

---
+++ day-2/output.tf
@@ -0,0 +1,4 @@
+output "nahaz" {
+    value = aws_instance.name.public_ip
+  
+}
---
+++ day-2/variable.tf
@@ -0,0 +1,13 @@
+variable "ami_id" {
+    description = "passing ami values"
+    default = ""
+    type = string
+  
+}
+
+variable "type" {
+    description = "pssing values to instance type"
+    default = ""
+    type = string
+  
+}
