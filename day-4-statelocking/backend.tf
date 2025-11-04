f33010824964613879fb0f6165f186f25346b6c6
nahaz <shaiknahaz@gmail.com>
2025-11-04 14:23:12 +0530

    update

--- day-4-statelocking/backend.tf
+++ day-4-statelocking/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key    = "terraform.tfstate"
    #use_lockfile = true # to use s3 native locking 1.19 version above
     region = "ap-south-1"
-    dynamodb_table = "veera" # any version we can use dynamodb locking 
+    dynamodb_table = "shennu" # any version we can use dynamodb locking 
     encrypt = true
   }
 }
