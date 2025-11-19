9eed6b3de87d68565875ae4398a9800e5106dfd0
shaiknahaz <shaiknahaz3141@gmail.com>
2025-11-19 16:27:20 +0530

    Update main.tf

--- day-3-statefile/main.tf
+++ day-3-statefile/main.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "name" { 
-    instance_type = "t3.micro"
-    region = "ap-southeast-2"
-     ami = var.ami_id
-     tags = {
-       Name = "sana"
-     }
+provider "aws" {
+  region = "ap-southeast-2"    # <-- Your region here
+}
 
+resource "aws_instance" "name" { 
+  instance_type = "t3.micro"   # t3.micro is Free Tier eligible
+  ami           = var.ami_id
+
+  tags = {
+    Name = "sana"
+  }
 }
 
 resource "aws_s3_bucket" "name" {
-    bucket = "sulthanpuuuriiii"
-  
+  bucket = "sulthanpuuuriiii"
 }
-
-
